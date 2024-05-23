4dfbfdbb4b6536823cf61d84e298d001e970e1d0
EduardaFarias <maria.eduarda.farias@ccc.ufcg.edu.br>
2024-05-23 17:50:00 -0300

    feat: entity

--- app/src/main/java/com/example/app/entity/User.java
+++ app/src/main/java/com/example/app/entity/User.java
@@ -1,25 +1,39 @@
 package com.example.app.entity;
 
 import java.time.LocalTime;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Data
+@Builder
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+
     @Email
+    @Column(nullable = false, unique = true)
     private String email;
+
     @Column(nullable = false)
     private String password;
+
     @Column(nullable = false)
     private String userName;
+
     @Column(nullable = false)
     private String nome;
+
     @Column(nullable = false)
     private LocalTime creationTime;
 }
