1975ac9799b327e03187e0f3b7550214eb43ba44
Vinicius <viniipvp@gmail.com>
2024-05-23 17:26:27 -0300

    entity

---
+++ app/src/main/java/com/example/app/entity/User.java
@@ -0,0 +1,23 @@
+package com.example.app.entity;
+
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+
+@Entity
+public class User {
+    @Id
+    @Email
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String userName;
+    @Column(nullable = false)
+    private String nome;
+    @Column(nullable = false)
+    private LocalTime creationTime;
+}
