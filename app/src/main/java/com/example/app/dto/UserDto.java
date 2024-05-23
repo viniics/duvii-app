df6025d3ed819d809732e1332de977de3561b146
Vinicius <viniipvp@gmail.com>
2024-05-23 17:57:51 -0300

    UserDTO Creation

---
+++ app/src/main/java/com/example/app/dto/UserDto.java
@@ -0,0 +1,40 @@
+package com.example.app.dto;
+
+import java.time.LocalTime;
+
+import com.example.app.entity.User;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Builder;
+import lombok.Data;
+
+@Builder
+@Data
+public class UserDto {
+    @Email
+    @NotNull
+    private String email;
+    @NotNull
+
+    private String password;
+
+    @NotNull
+    private String userName;
+
+    @NotNull
+    private String nome;
+
+    @NotNull
+    private LocalTime creationTime;
+
+    public User convertDtoToUser(){
+            User newUser = new User();
+            newUser.setEmail(email);
+            newUser.setPassword(password);
+            newUser.setUserName(userName);
+            newUser.setNome(nome);
+            newUser.setCreationTime(LocalTime.now());
+            return newUser;
+        }
+}
--- app/src/main/java/com/example/app/entity/User.java
+++ app/src/main/java/com/example/app/entity/User.java
@@ -1,7 +1,6 @@
 package com.example.app.entity;
 
 import java.time.LocalTime;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 
 import jakarta.persistence.*;
